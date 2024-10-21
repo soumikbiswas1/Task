5b3949a4f648817f1caf4c6bcc6f83f5edb7e616
soumikbiswas1 <soumikjiswas@gmail.com>
2024-10-21 18:20:07 +0530

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import logo from '../../assets/images/logo.png';
+//import logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
